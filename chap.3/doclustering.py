b2c3ef677b759c6ff9199d1507519ac2f1326132
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2012-10-28 18:03:59 +0900

    add k-Means clustering code

--- chap.3/doclustering.py
+++ chap.3/doclustering.py
@@ -2,5 +2,24 @@ import clusters
 
 
 blognames, words, data = clusters.readfile('blogdata.txt')
+
+print '--------------------'
+print 'hierarchical clustering'
+print '--------------------'
+
 clust = clusters.hcluster(data)
 clusters.printclust(clust, labels=blognames)
+
+
+print '--------------------'
+print 'k-Means clustering'
+print '--------------------'
+
+numclusters = 3
+clust = clusters.kcluster(data, k=numclusters)
+for i in range(numclusters):
+    print 'cluster[%d]:' % i
+    currentcluster = clust[i]
+    for r in currentcluster:
+        print '\t%s' % blognames[r]
+    print
